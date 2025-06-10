546b7fe781d6e7e1191b497acb7667575a516f06
Mohammad Mohammad <113510708+theKageEth@users.noreply.github.com>
2025-06-10 04:01:13 +0100

    Update page.jsx 2

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -2,7 +2,33 @@ import OrderData from "@/components/orderPage/OrderData";
 import { getAllOrders } from "@/lib/data";
 
 const OrdersPage = async () => {
-  // const data = await getAllOrders();
+ const data = [
+  {
+    _id: 'order-1',
+    userId: 'user-123',
+    username: 'John Doe',
+    totalAmount: 45.50,
+    paymentMethod: 'Card',
+    status: 'Preparing',
+  },
+  {
+    _id: 'order-2',
+    userId: 'user-456',
+    username: 'Jane Smith',
+    totalAmount: 27.99,
+    paymentMethod: 'Cash',
+    status: 'Delivered',
+  },
+  {
+    _id: 'order-3',
+    userId: 'user-789',
+    username: 'Alex Johnson',
+    totalAmount: 15.00,
+    paymentMethod: 'Card',
+    status: 'Pending',
+  },
+];
+
 
   return (
     <>
